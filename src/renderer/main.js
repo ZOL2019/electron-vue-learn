5297eacd6bad7045bea1c796eed5fb469fb274c5
libin.zhou <binteam2018@163.com>
2018-12-07 18:00:31 +0800

    element-ui success

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,11 +4,15 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+/* element-ui */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+/* element-ui */
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
--- src/renderer/views/IndexPage.vue
+++ src/renderer/views/IndexPage.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-        index-page
-    <span>{{showPage}}</span>
-    </div>
+    <el-container>
+        <el-header>
+            <el-row>
+                <el-col :span="4">index-page</el-col>
+                <el-col :span="16">Table-Operation</el-col>
+                <el-col :span="4"><span>{{showPage}}</span></el-col>
+            </el-row>
+        </el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
